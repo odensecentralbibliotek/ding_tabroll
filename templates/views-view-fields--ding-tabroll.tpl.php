1531d4af173a4a71bb0eb9964bc855dcba9473e9
Rune Schjellerup Philosof <rune.git@philosof.dk>
2013-10-07 09:52:57 +0200

    Allow having no link.

--- templates/views-view-fields--ding-tabroll.tpl.php
+++ templates/views-view-fields--ding-tabroll.tpl.php
@@ -1,8 +1,20 @@
 <div class="image">
-  <?php print l($fields['field_ding_tabroll_image']->content, 'node/' . $fields['field_ding_tabroll_link']->raw, array('html' => TRUE)); ?>
+  <?php
+  if (empty($fields['field_ding_tabroll_link']->raw)) {
+    print $fields['field_ding_tabroll_image']->content;
+  } else {
+    print l($fields['field_ding_tabroll_image']->content, 'node/' . $fields['field_ding_tabroll_link']->raw, array('html' => TRUE));
+  }
+  ?>
 </div>
 
 <div class="info">
-  <h3><?php print l($fields['title']->content, 'node/' . $fields['field_ding_tabroll_link']->raw); ?></h3>
+  <h3><?php
+  if (empty($fields['field_ding_tabroll_link']->raw)) {
+    print $fields['title']->content;
+  } else {
+    print l($fields['title']->content, 'node/' . $fields['field_ding_tabroll_link']->raw);
+  }
+  ?></h3>
   <p><?php print $fields['field_ding_tabroll_lead']->content; ?></p>
 </div>
