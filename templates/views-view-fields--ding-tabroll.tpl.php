42917c46bfb0c2e75e975fc97f6d8dfb57bb15aa
Rune Schjellerup Philosof <rune.git@philosof.dk>
2013-11-26 14:25:54 +0100

    Add possibility to link to external and add anchor to internal.

--- templates/views-view-fields--ding-tabroll.tpl.php
+++ templates/views-view-fields--ding-tabroll.tpl.php
@@ -1,19 +1,30 @@
+<?php
+if (!empty($fields['field_ding_tabroll_link']->raw)) {
+  $link = 'node/' . $fields['field_ding_tabroll_link']->raw;
+  if (!empty($fields['field_ding_tabroll_anchor']->content)) {
+    $link .= '#' . $fields['field_ding_tabroll_anchor']->content;
+  }
+}
+if (empty($link) && isset($fields['field_ding_tabroll_external'])) {
+  $link = $fields['field_ding_tabroll_external']->content;
+}
+?>
 <div class="image">
   <?php
-  if (empty($fields['field_ding_tabroll_link']->raw)) {
+  if (empty($link)) {
     print $fields['field_ding_tabroll_image']->content;
   } else {
-    print l($fields['field_ding_tabroll_image']->content, 'node/' . $fields['field_ding_tabroll_link']->raw, array('html' => TRUE));
+    print l($fields['field_ding_tabroll_image']->content, $link, array('html' => TRUE));
   }
   ?>
 </div>
 
 <div class="info">
   <h3><?php
-  if (empty($fields['field_ding_tabroll_link']->raw)) {
+  if (empty($link)) {
     print $fields['title']->content;
   } else {
-    print l($fields['title']->content, 'node/' . $fields['field_ding_tabroll_link']->raw);
+    print l($fields['title']->content, $link);
   }
   ?></h3>
   <p><?php print $fields['field_ding_tabroll_lead']->content; ?></p>
