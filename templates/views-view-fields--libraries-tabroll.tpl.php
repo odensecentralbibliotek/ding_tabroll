efa29edf2fd00cd5241cd39d5724752132e20b93
Amer <impara1@gmail.com>
2014-05-02 12:47:42 +0200

    Fixed target _blank on external link for libraries

--- templates/views-view-fields--libraries-tabroll.tpl.php
+++ templates/views-view-fields--libraries-tabroll.tpl.php
@@ -1,19 +1,38 @@
+<?php
+if ($row->_field_data['nid']['entity']->field_ding_tabroll_external['und'][0]['attributes']['target'] === '_blank'){
+       $target = array();
+       $target['attributes']['target'] = '_blank';
+}
+if (!empty($fields['field_ding_tabroll_link']->raw)) {
+  $link = 'node/' . $fields['field_ding_tabroll_link']->raw;
+  if (!empty($fields['field_ding_tabroll_anchor']->content)) {
+    $link .= '#' . $fields['field_ding_tabroll_anchor']->content;
+  }
+}
+if (empty($link) && isset($fields['field_ding_tabroll_external'])) {
+   $link = $fields['field_ding_tabroll_external']->content;
+}
+?>
 <div class="image">
   <?php
-  if (empty($fields['field_ding_tabroll_link']->raw)) {
+  if (empty($link)) {
     print $fields['field_ding_tabroll_image']->content;
-  } else {
-    print l($fields['field_ding_tabroll_image']->content, 'node/' . $fields['field_ding_tabroll_link']->raw, array('html' => TRUE));
+  } elseif(isset($target)) {
+    print l($fields['field_ding_tabroll_image']->content, $link, $target, array('html' => TRUE));
+  }else{
+      print l($fields['field_ding_tabroll_image']->content, $link, array('html' => TRUE));
   }
   ?>
 </div>
 
 <div class="info">
   <h3><?php
-  if (empty($fields['field_ding_tabroll_link']->raw)) {
-    print $fields['title'];
-  } else {
-    print l($fields['title']->content, 'node/' . $fields['field_ding_tabroll_link']->raw);
+  if (empty($link)) {
+    print $fields['title']->content;
+  } elseif(isset($target)) {
+      print l($fields['title']->content, $link, $target, array('html' => TRUE));    
+  }else {
+      print l($fields['title']->content, $link, array('html' => TRUE)); 
   }
   ?></h3>
   <p><?php print $fields['field_ding_tabroll_lead']->content; ?></p>
